7865bb79e11d544dfb152009e56ebe85ffe3a7eb
hadi-a-azeez <hadikoduvally@gmail.com>
2021-08-29 01:02:25 -0700

    input added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import Input from "./components/Input";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Input placeholder="Enter your name" value={name} onChange={setName} />
     </div>
   );
 }
